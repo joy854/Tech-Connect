be770338297225b7995dc2ecdffba73c09229cc6
Joy Sinha <39700727+joy854@users.noreply.github.com>
2020-04-26 03:39:51 +0530

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Hero from '../components/Hero';
 import { Link } from 'react-router-dom';
 import Service from '../components/Service';
+import { UserContext } from '../context/user';
 export default function App() {
+  const { user } = React.useContext(UserContext);
   return (
     <div>
       <Hero>
-        <Link to='/products' className='BUTTON_LAI'>
-          our products
+        <Link to='/login' className='BUTTON_LAI'>
+          {user.id && <span>Welcome!</span>}
+          {!user.id && <span>Lets's Begin!</span>}
         </Link>
       </Hero>
       <Service />
